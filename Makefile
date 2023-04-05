6267ff84fcf87814c85586e60be63f9fde0e1955
Mikhail Sliusarev <mik3weider@gmail.com>
2023-04-05 19:29:17 +0200

    Move UI to a separate file

--- Makefile
+++ Makefile
@@ -7,7 +7,7 @@ LD = g++
 LDFLAGS = `wx-config --libs` -lcurl
 
 # Source files
-SRCS = main.cpp
+SRCS = main.cpp ui.cpp
 
 # Object files
 OBJS = $(SRCS:.cpp=.o)
---
+++ ui.h
@@ -0,0 +1,26 @@
+#include <wx/frame.h>
+#include <wx/button.h>
+#include <wx/textctrl.h>
+
+#include <curl/curl.h>
+#include <wx/log.h>
+#include <wx/wx.h>
+#include <wx/frame.h>
+#include <wx/button.h>
+
+#include <fstream>
+#include <string>
+
+#include <nlohmann/json.hpp>
+
+class MainFrame : public wxFrame
+{
+public:
+  MainFrame(const wxString &title);
+
+private:
+  void OnSubmit(wxCommandEvent &event);
+
+  wxTextCtrl *inputField;
+  wxButton *submitButton;
+};
